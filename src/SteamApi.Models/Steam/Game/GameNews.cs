6befed6771270efae9f99847b117066ad4995851
sgnome <lilchim@protonmail.com>
2025-08-06 13:36:30 -0400

    Update GameNews.cs and GameNewsResponse.cs
    Add JsonPropertyNames and update Date type to ulong.

--- src/SteamApi.Models/Steam/Game/GameNews.cs
+++ src/SteamApi.Models/Steam/Game/GameNews.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace SteamApi.Models.Steam.Game;
 
 /// <summary>
@@ -8,55 +10,66 @@ public class GameNews
     /// <summary>
     /// The unique identifier of the news item
     /// </summary>
+    [JsonPropertyName("gid")]
     public string Gid { get; set; } = string.Empty;
     
     /// <summary>
     /// The title of the news item
     /// </summary>
+    [JsonPropertyName("title")]
     public string Title { get; set; } = string.Empty;
     
     /// <summary>
     /// The URL of the news item
     /// </summary>
+    [JsonPropertyName("url")]
     public string Url { get; set; } = string.Empty;
     
     /// <summary>
     /// Whether the news item is external
     /// </summary>
+    [JsonPropertyName("is_external_url")]
     public bool IsExternalUrl { get; set; }
     
     /// <summary>
     /// The author of the news item
     /// </summary>
+    [JsonPropertyName("author")]
     public string Author { get; set; } = string.Empty;
     
     /// <summary>
     /// The contents of the news item
     /// </summary>
+    [JsonPropertyName("contents")]
     public string Contents { get; set; } = string.Empty;
     
     /// <summary>
     /// The label of the news item
     /// </summary>
+    [JsonPropertyName("feedlabel")]
     public string FeedLabel { get; set; } = string.Empty;
     
     /// <summary>
     /// The date the news item was published
     /// </summary>
-    public DateTime Date { get; set; }
+    [JsonPropertyName("date")]
+    public ulong Date { get; set; }
     
     /// <summary>
     /// The feed name
     /// </summary>
+    [JsonPropertyName("feedname")]
     public string FeedName { get; set; } = string.Empty;
     
     /// <summary>
     /// The feed type
     /// </summary>
+    [JsonPropertyName("feed_type")]
     public int FeedType { get; set; }
     
     /// <summary>
     /// The app ID of the game
     /// </summary>
+    [JsonPropertyName("appid")]
     public int AppId { get; set; }
 } 
--- src/SteamApi.Models/Steam/Responses/GameNewsResponse.cs
+++ src/SteamApi.Models/Steam/Responses/GameNewsResponse.cs
@@ -1,4 +1,5 @@
 using SteamApi.Models.Steam.Game;
+using System.Text.Json.Serialization;
 
 namespace SteamApi.Models.Steam.Responses;
 
@@ -8,17 +9,32 @@ namespace SteamApi.Models.Steam.Responses;
 public class GameNewsResponse
 {
     /// <summary>
+    /// App news wrapper
+    /// </summary>
+    [JsonPropertyName("appnews")]
+    public AppNews AppNews { get; set; } = new();
+}
+
+/// <summary>
+/// App news information
+/// </summary>
+public class AppNews
+{
+    /// <summary>
     /// The application ID
     /// </summary>
+    [JsonPropertyName("appid")]
     public int AppId { get; set; }
     
     /// <summary>
     /// List of news items
     /// </summary>
+    [JsonPropertyName("newsitems")]
     public List<GameNews> NewsItems { get; set; } = new();
     
     /// <summary>
     /// The number of news items returned
     /// </summary>
+    [JsonPropertyName("count")]
     public int Count { get; set; }
 } 
